8565bdba0b4d8872f9ffddedf921abcbafaaeb9f
Morgan <morganfolz@gmail.com>
2022-08-16 13:59:21 -0700

    Rev 1.0

--- js/custom.js
+++ js/custom.js
@@ -415,3 +415,63 @@ POTENZA.searchbar = function () {
 })(jQuery);
 
 
+
+// SIMPLE CAROUSEL 1
+let slideIndex = 1;
+showSlides1(slideIndex);
+
+// Next/previous controls
+function plusSlides1(n) {
+  showSlides1(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide1(n) {
+  showSlides1(slideIndex = n);
+}
+
+function showSlides1(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides1");
+  let dots = document.getElementsByClassName("dot1");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// SIMPLE CAROUSEL 2
+// let slideIndex = 1;
+showSlides2(slideIndex);
+
+// Next/previous controls
+function plusSlides2(n) {
+  showSlides2(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide2(n) {
+  showSlides2(slideIndex = n);
+}
+
+function showSlides2(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides2");
+  let dots = document.getElementsByClassName("dot2");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
